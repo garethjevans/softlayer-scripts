8c2b6a761681c44a2cc3e76a00f17251d5c92623
Gareth Evans <garethjevans@users.noreply.github.com>
2015-06-13 16:58:01 +0100

    Corrected escaping for generated scripts

--- install-kubernetes-master.sh
+++ install-kubernetes-master.sh
@@ -16,9 +16,9 @@ cat << EOF > start-master.sh
 #!/bin/sh
 
 for SERVICES in etcd kube-apiserver kube-controller-manager kube-scheduler; do
-    systemctl restart $$SERVICES
-    systemctl enable $$SERVICES
-    systemctl status $$SERVICES
+    systemctl restart \$SERVICES
+    systemctl enable \$SERVICES
+    systemctl status \$SERVICES
 done
 EOF
 chmod +x start-master.sh
@@ -27,7 +27,7 @@ cat << EOF > stop-master.sh
 #!/bin/sh
 
 for SERVICES in etcd kube-apiserver kube-controller-manager kube-scheduler; do
-    systemctl stop $$SERVICES
+    systemctl stop \$SERVICES
 done
 EOF
 chmod +x stop-master.sh
